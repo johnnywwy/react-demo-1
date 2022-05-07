46d8bb817e10ceb175607b94bd1369a708b8a8a6
johnnywwy <johnnywwy@gmail.com>
2022-05-07 22:54:11 +0800

    react 使用 state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react'
 import './style.css'
 
 function App() {
-  const money = 100
   return (
     <div>
       <div className="App">
         爸爸
-        <Son messageForSon={money}/>
+        <Son/>
       </div>
-      <hr/>
     </div>
 
   );
@@ -17,27 +15,38 @@ function App() {
 
 class Son extends React.Component {
   constructor() {
-    super();
+    super()
     this.state = {
       n: 0
     }
   }
+  add() {
+    // this.setState({n: this.state.n += 1})
+    //  牛逼的前端这样写
+    this.setState((state) => {
+      const n = state.n + 1
+      console.log(n)
+      return {n}
+    })
+  }
+
   render() {
-    const money = 20;
     return (
       <div className="Son">
-        儿子：我收到了爸爸给我的 {this.props.messageForSon} 元钱
-        <Grandson messageForGrandSon={money}/>
+        儿子 n: {this.state.n}
+        <button onClick={() => this.add()}>+1</button>
+        <GrandSon/>
       </div>
     )
   }
 }
 
-const Grandson = (props) => {
-  // const [n, setN] = React.useState(0)
+const GrandSon = () => {
+  const [n, setN] = React.useState(0)
   return (
     <div className="Grandson">
-      孙子：我收到爷爷给我的 {props.messageForGrandSon} 红包
+      孙子 n：{n}
+      <button onClick={() => setN(n + 1)}>+1</button>
     </div>
   )
 }
