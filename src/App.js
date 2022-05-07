da7d82d2572eb37390fab459bd480b5dfe7ded4e
johnnywwy <johnnywwy@gmail.com>
2022-05-07 16:48:17 +0800

    react 遍历数组

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
 function App() {
   return (
-    <div className="App">
-      App组件
+    <div>
+      <h1>App组件</h1>
+      <Component numbers={[1,2,3,4,5,6,7,8,9,0]}/>
     </div>
   );
+}
+
+const Component = (props) => {
+  return (<div>
+    { props.numbers.map((n,index) => {
+      return <div> 下标 {index} : 值为{n}</div>
+    }) }
+  </div>)
 }
 
 export default App;
